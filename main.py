adef3b2ec7d3228cc40056f46c8957f1d0eed16e
petrykivd <petrykiv.dmytro19@gmail.com>
2024-03-03 13:19:38 +0200

    .

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
+from database import create_tables, delete_tables
+from router import router as tasks_router
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await delete_tables()
+    print("Database cleaned.")
+    await create_tables()
+    print("Database is ready!")
+    yield
+    print("Shutdown")
 
+app = FastAPI(lifespan=lifespan)
+app.include_router(tasks_router)
 
-@app.get("/home")
-def get_home():
-    return "Hello World!"
